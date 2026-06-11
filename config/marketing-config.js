76f98351f3a85e32939562dfc64fc15893170841
h1987121788 <130131251+h1987121788@users.noreply.github.com>
2026-06-11 20:32:02 +0800

    Create marketing-config.js

---
+++ config/marketing-config.js
@@ -0,0 +1,9 @@
+window.bingoMarketingConfig = {
+  ga4MeasurementId: "G-4W8V1TXJGH",
+  metaPixelId: "2252311015604509",
+  crmWebhookUrl: "https://script.google.com/macros/s/AKfycbw42c1sFtLmEXKmP-iyqut3udvDV4aP8X10KAh3G80nsNptYg6Q6Z9-XgZ8uGZHf2AlwQ/exec",
+  requireMarketingConsent: true,
+  googleSearchConsoleVerification: "GohRVRiXIyc-ETT24wVPRFC2hK0ZxReiS0LjBT9DJSA",
+  siteUrl: "https://www.bingofabric.com/",
+  sourceLabel: "Bingo Textile website"
+};
