944ee90b265c5decba6c6ad6307ac0d23a5dc81b
h1987121788 <130131251+h1987121788@users.noreply.github.com>
2026-06-13 22:11:49 +0800

    Add CRM webhook submit token config

--- config/marketing-config.js
+++ config/marketing-config.js
@@ -2,6 +2,7 @@ window.bingoMarketingConfig = {
   ga4MeasurementId: "G-4W8V1TXJGH",
   metaPixelId: "2252311015604509",
   crmWebhookUrl: "https://script.google.com/macros/s/AKfycbw42c1sFtLmEXKmP-iyqut3udvDV4aP8X10KAh3G80nsNptYg6Q6Z9-XgZ8uGZHf2AlwQ/exec",
+  crmSubmitToken: "6ccf6366ee2d596d83a321c5310505ab7ebadef8593ef89a",
   requireMarketingConsent: true,
   googleSearchConsoleVerification: "GohRVRiXIyc-ETT24wVPRFC2hK0ZxReiS0LjBT9DJSA",
   siteUrl: "https://www.bingofabric.com/",
