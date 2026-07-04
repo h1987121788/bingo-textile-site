60f671b9186387ea5105fc2b25ac42d0dc885a87
h1987121788 <h1987121788@users.noreply.github.com>
2026-07-04 22:44:30 +0800

    Enable production CRM webhook config

--- config/marketing-config.js
+++ config/marketing-config.js
@@ -1,8 +1,8 @@
 window.bingoMarketingConfig = {
   ga4MeasurementId: "G-XXXXXXXXXX",
   metaPixelId: "PIXEL_ID",
-  crmWebhookUrl: "PASTE_WEBHOOK_URL",
-  crmSubmitToken: "SAME_TOKEN_AS_APPS_SCRIPT",
+  crmWebhookUrl: "https://script.google.com/macros/s/AKfycbw42c1sFtLmEXKmP-iyqut3udvDV4aP8X10KAh3G80nsNptYg6Q6Z9-XgZ8uGZHf2AlwQ/exec",
+  crmSubmitToken: "3e01990408120112fc05fe449796584e18674149bf6beae70283c743f4d21805",
   requireMarketingConsent: true,
   googleSearchConsoleVerification: "PASTE_SEARCH_CONSOLE_VERIFICATION",
   siteUrl: "https://www.bingofabric.com/",
